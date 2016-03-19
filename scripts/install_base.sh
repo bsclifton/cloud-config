3f746281bd9be140102243f92734662560cacb6b
Brian Clifton <brian@clifton.me>
2016-03-19 14:42:38 -0700

    now purges packages after removal

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -6,3 +6,4 @@ sudo apt-get upgrade -y
 sudo apt-get install -y git cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs
 
 sudo apt-get remove -y vim-tiny ppp pppconfig pppoeconf nano
+dpkg --list |grep "^rc" | cut -d " " -f 3 | xargs sudo dpkg --purge
