ecba86d15ec41663746f377eb1d82d8b28fbe8c0
Brian Clifton <brian@clifton.me>
2016-03-20 11:03:03 -0700

    Base install now installs MySQL w/ random pw and PHP

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -3,7 +3,23 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y git cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs
+#-------------------------------------------------------------
+# Package install
+#-------------------------------------------------------------
+# curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs
+#>>> for Ruby / nginx / Passenger
+#
+# mysql-server mysql-client
+#>>> MySQL installed with a random root password
+#
+# php5-fpm
+#>>> for PHP
 
+RANDOM_PW=$(openssl rand -base64 32)
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $RANDOM_PW"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $RANDOM_PW"
+sudo apt-get install -y cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs mysql-server mysql-client php5-fpm
+
+# Remove unnecessary packages
 sudo apt-get remove -y vim-tiny ppp pppconfig pppoeconf nano
 dpkg --list |grep "^rc" | cut -d " " -f 3 | xargs sudo dpkg --purge
