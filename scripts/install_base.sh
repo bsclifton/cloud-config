ddb349d304613771067f19b1e97e9aaa9920e8a3
Brian Clifton <brian@clifton.me>
2016-03-20 14:26:11 -0700

    Starting to bring up the WordPress blog...

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -12,13 +12,13 @@ sudo apt-get upgrade -y
 # mysql-server mysql-client
 #>>> MySQL installed with a random root password
 #
-# php5-fpm
+# php5-fpm php5-mysql php5-curl php5-gd
 #>>> for PHP
 
 RANDOM_PW=$(openssl rand -base64 32)
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $RANDOM_PW"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $RANDOM_PW"
-sudo apt-get install -y cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs mysql-server mysql-client php5-fpm
+sudo apt-get install -y cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs mysql-server mysql-client php5-fpm php5-mysql php5-curl php5-gd
 
 # Remove unnecessary packages
 sudo apt-get remove -y vim-tiny ppp pppconfig pppoeconf nano
