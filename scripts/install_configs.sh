db80c2883d6611f045c1b607be4b0dd48cef16e9
Brian Clifton <brian@clifton.me>
2016-03-26 01:38:18 -0700

    Updated configs after live testing w/ hostfile. Updated DNS, site is now live.

--- scripts/install_configs.sh
+++ scripts/install_configs.sh
@@ -6,10 +6,10 @@ sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.ba
 sudo cp /etc/nginx/snippets/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf.bak
 
 # Copy configs
-cp ../etc/php5/fpm/php.ini /etc/php5/fpm/php.ini
-cp ../etc/nginx/conf.d/blog.conf /etc/nginx/conf.d/
-cp ../etc/nginx/sites-available/default /etc/nginx/sites-available/default
-cp ../etc/nginx/snippets/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf
+sudo cp ../etc/php5/fpm/php.ini /etc/php5/fpm/php.ini
+sudo cp ../etc/nginx/conf.d/*.conf /etc/nginx/conf.d/
+sudo cp ../etc/nginx/sites-available/default /etc/nginx/sites-available/default
+sudo cp ../etc/nginx/snippets/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf
 
 # Restart services
 sudo /etc/init.d/php5-fpm restart
