331bee9c4b44fccbc23f38b40b6a1d079e9915f9
Brian Clifton <brian@clifton.me>
2017-09-01 08:45:10 -0700

    update base packages

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -18,7 +18,7 @@ sudo apt-get upgrade -y
 RANDOM_PW=$(openssl rand -base64 32)
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $RANDOM_PW"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $RANDOM_PW"
-sudo apt-get install -y cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs mysql-server mysql-client php5-fpm php5-mysql php5-curl php5-gd
+sudo apt-get install -y cowsay curl build-essential libssl-dev libreadline-dev zlib1g-dev nodejs mysql-server mysql-client php5-fpm php5-mysql php5-curl php5-gd zip unzip
 
 # Remove unnecessary packages
 sudo apt-get remove -y vim-tiny ppp pppconfig pppoeconf nano
