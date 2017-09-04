a1a3f48ea509f61727af20efbd99d1fe9ba388a4
Brian Clifton <brian@clifton.me>
2017-09-03 23:52:16 -0700

    More updates after a second run

--- scripts/install_passenger.sh
+++ scripts/install_passenger.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Steps taken from https://www.phusionpassenger.com/library/install/nginx/install/oss/trusty/
+# Steps taken from https://www.phusionpassenger.com/library/install/nginx/install/oss/xenial/
 
 # Install our PGP key and add HTTPS support for APT
+sudo apt-get install -y dirmngr gnupg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install -y apt-transport-https ca-certificates
 
 # Add our APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
 
 # Install Passenger + Nginx
--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Install rbenv - https://github.com/rbenv/rbenv
-pushd .
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-cd ~/.rbenv && src/configure && make -C src
-popd
+if [ ! -f ~/.rbenv/bin/rbenv ]; then
+  pushd .
+  rm -rf ~/.rbenv
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  cd ~/.rbenv && src/configure && make -C src
+  popd
+fi
 
 # Install ruby-build - https://github.com/rbenv/ruby-build#readme
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # Install latest stable Ruby version
-source ~/.profile
+source ~/.bashrc
 RUBY_CONFIGURE_OPTS=--disable-install-doc rbenv install 2.4.1
 rbenv global 2.4.1
 
