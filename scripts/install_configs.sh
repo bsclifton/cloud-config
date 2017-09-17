dc09b5eb9c13dd40e2faf18a00dc5d9a10556ade
Brian Clifton <brian@clifton.me>
2017-09-17 00:05:01 -0700

    path update for configs

--- scripts/install_configs.sh
+++ scripts/install_configs.sh
@@ -6,15 +6,15 @@ sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.ba
 sudo cp /etc/nginx/snippets/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf.bak
 
 # Copy configs
-sudo cp ../etc/php5/fpm/php.ini /etc/php/7.0/fpm/php.ini
-sudo cp ../etc/nginx/conf.d/*.conf /etc/nginx/conf.d/
-sudo cp ../etc/nginx/sites-available/default /etc/nginx/sites-available/default
-sudo cp ../etc/nginx/snippets/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf
+sudo cp ./etc/php5/fpm/php.ini /etc/php/7.0/fpm/php.ini
+sudo cp ./etc/nginx/conf.d/*.conf /etc/nginx/conf.d/
+sudo cp ./etc/nginx/sites-available/default /etc/nginx/sites-available/default
+sudo cp ./etc/nginx/snippets/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf
 
 # files used by SSL configurations
 sudo mkdir -p /srv/
 sudo openssl dhparam -out /srv/dhparam.pem 2048
-sudo cp ../srv/gd_bundle-g2-g1.crt /srv/gd_bundle-g2-g1.crt
+sudo cp ./srv/gd_bundle-g2-g1.crt /srv/gd_bundle-g2-g1.crt
 
 # Restart services
 sudo service php7.0-fpm restart
